f298834e51604134bcb6159a674cf70db6f6bc0b
Xavier <xavierh93@hotmail.com>
2023-04-05 19:50:32 -0400

    setup logim page template

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,11 +2,7 @@ import { NextPage } from 'next'
 import Layout from '@/components/Layout'
 
 const Login: NextPage = () => {
-  return (
-    <Layout>
-      <h1>Login</h1>
-    </Layout>
-  )
+  return <Layout></Layout>
 }
 
 export default Login
