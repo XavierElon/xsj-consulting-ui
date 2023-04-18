9e8942bd938446b247c7e9f6a5601fd5276c2fd5
Xavier Hollingsworth <xavierelon93@gmail.com>
2023-04-18 16:30:13 -0400

    more navbar styling; setup routing

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next'
+import Layout from '@/components/Layout'
+
+const SignUp: NextPage = () => {
+  return (
+    <>
+      <Layout></Layout>
+    </>
+  )
+}
+
+export default SignUp
--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -1,9 +1,10 @@
 'use client'
-
 import React from 'react'
+import Link from 'next/link'
 import { CgMenuGridR } from 'react-icons/cg'
 import { XMarkIcon } from '@heroicons/react/24/outline'
 import classNames from 'classnames'
+import { Button } from 'react-bootstrap'
 
 type Props = {
   open: boolean
@@ -19,17 +20,44 @@ const Navbar = (props: Props) => {
         'w-full fixed z-10 px-4 h-16': true, //positioning & styling
       })}
     >
-      <div className="absolute right-1 font-bold text-lg">DevGru</div>
-      <div className="flex-grow"></div>
       {props.open ? (
-        <button onClick={props.handleClick}>
+        <button onClick={props.handleClick} className="z-50">
           <XMarkIcon className="h-6 w-6" />
         </button>
       ) : (
-        <button onClick={props.handleClick}>
-          <CgMenuGridR className="absolute left-1 h-6 w-6" />
-        </button>
+        <div className="flex flex-col justify-start px-10">
+          <button onClick={props.handleClick}>
+            <CgMenuGridR className="h-6 w-6" />
+          </button>
+        </div>
       )}
+      <Link href="/">
+        <div className="flex flex-col justify-start font-bold text-lg text-white px-5">
+          DevGru
+        </div>
+      </Link>
+      <div className="flex-grow"></div>
+
+      <div className="flex flex-col justify-end px-10">
+        <Link href="/login">
+          <Button
+            variant="light"
+            className="bg-slate-50 hover:bg-slate-200 px-2 py-2 rounded-lg text-black"
+          >
+            Login
+          </Button>
+        </Link>
+      </div>
+      <div className="flex flex-col justify-end px-10">
+        <Link href="/signup">
+          <Button
+            variant="primary"
+            className="bg-sky-400 hover:bg-sky-600 px-2 py-2 rounded-lg text-white"
+          >
+            Sign Up
+          </Button>
+        </Link>
+      </div>
     </nav>
   )
 }
