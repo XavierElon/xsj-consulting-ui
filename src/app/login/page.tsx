300bc5d8cb72caf403ff0ccaf0867ded6efbdcb8
Xavier Hollingsworth <xavierelon93@gmail.com>
2023-04-19 16:10:31 -0400

    add email and password form fields

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,7 @@
 import { NextPage } from 'next'
 import Layout from '@/components/Layout'
 import LoginModal from '@/components/modals/LoginModal'
+import '../../css/ellipsis.css'
 
 const Login: NextPage = () => {
   return (
@@ -10,8 +11,15 @@ const Login: NextPage = () => {
           <div className="flex w-1/2 bg-white mx-auto items-center justify-center">
             <LoginModal className=""></LoginModal>
           </div>
-          <div className="w-1/2 bg-[#6AA7F8]">
+          {/* <div className="w-1/2 bg-[#6AA7F8]">
             <p>Right-side content goes here.</p>
+          </div> */}
+          <div className="w-1/2 bg-gradient-to-t from-[#77cafe] to-[#0069FF] p-8">
+            <div className="relative">
+              <div className="mask-ellipsis">
+                Some text that is long enough to be truncated with an ellipsis
+              </div>
+            </div>
           </div>
         </div>
       </Layout>
--- src/components/modals/LoginModal.tsx
+++ src/components/modals/LoginModal.tsx
@@ -58,22 +58,32 @@ const LoginModal = (props: any) => {
         <h2 style={headerStyle} className="text-black text-center">
           Log in to your account
         </h2>
-        <TextField
-          required
-          id="outlined-required"
-          label="Required"
-          size="small"
-          defaultValue="Hello World"
-          className="transform hover:scale-110 transition-all duration-300 w-72"
-        />
-        <TextField
-          id="outlined-password-input"
-          label="Password"
-          type="password"
-          size="small"
-          autoComplete="current-password"
-          className="transform hover:scale-110 transition-all duration-300 w-80"
-        />
+        <form>
+          <p>
+            <span className="text-[#0069FF]">email </span>
+            <span className="text-red-600 relative top-1">*</span>
+          </p>
+          <TextField
+            required
+            id="outlined-required"
+            // label="Required"
+            size="small"
+            defaultValue="Hello World"
+            className="transform hover:scale-110 transition-all duration-300 w-80 justify-center"
+          />
+          <p>
+            <span className="text-[#0069FF]">password </span>
+            <span className="text-red-600 relative top-1">*</span>
+          </p>
+          <TextField
+            id="outlined-password-input"
+            // label="Password"
+            type="password"
+            size="small"
+            autoComplete="current-password"
+            className="transform hover:scale-110 transition-all duration-300 w-80 justify-center relative"
+          />
+        </form>
       </div>
     </div>
   )
@@ -82,8 +92,8 @@ const LoginModal = (props: any) => {
 export default LoginModal
 
 const containerStyle: CSSProperties = {
-  width: '478px',
-  height: '524px',
+  width: '440px',
+  height: '496px',
   marginLeft: '3%',
 }
 
