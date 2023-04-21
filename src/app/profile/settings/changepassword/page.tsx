61b22d225cf6df8809ff6705163c9de75a635e05
Xavier Hollingsworth <xavierelon93@gmail.com>
2023-04-21 09:21:28 -0400

    start change password page

---
+++ src/app/profile/settings/changepassword/page.tsx
@@ -0,0 +1,37 @@
+'use client'
+import React from 'react'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import Link from 'next/link'
+import { auth } from '@/firebase/firebase'
+import Layout from '@/components/Layout'
+import Forbidden from '../../../forbidden/page'
+import ChangePasswordModal from '@/components/modals/ChangePasswordModal'
+
+const ChangePassword = () => {
+  const [user] = useAuthState(auth)
+
+  return (
+    <>
+      {user ? (
+        <>
+          <Layout>
+            <div className="flex min-h-screen">
+              <div className="w-1/2 bg-gradient-to-t from-[#77cafe] to-[#0069FF] p-8">
+                <div className="relative">
+                  <div className="mask-ellipsis"></div>
+                </div>
+              </div>
+              <div className="flex w-1/2 bg-white mx-auto items-center justify-center">
+                <ChangePasswordModal className=""></ChangePasswordModal>
+              </div>
+            </div>
+          </Layout>
+        </>
+      ) : (
+        <Forbidden />
+      )}
+    </>
+  )
+}
+
+export default ChangePassword
