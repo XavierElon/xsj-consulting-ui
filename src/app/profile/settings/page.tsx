1d8473af07e57458617006aef67f0d89b71e4a80
Xavier Hollingsworth <xavierelon93@gmail.com>
2023-04-27 13:11:08 -0400

    fix forbidden bug

--- src/app/profile/settings/page.tsx
+++ src/app/profile/settings/page.tsx
@@ -1,11 +1,88 @@
 'use client'
+import { AuthStateContext } from '@/context/AuthContext'
+import { useAuthorization } from '@/hooks/useAuthorization'
+import { useContext, useEffect } from 'react'
+import Link from 'next/link'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle'
+import Forbidden from '@/app/forbidden/page'
 import Layout from '@/components/Layout'
-import { Navbar } from 'react-bootstrap'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { auth } from '@/firebase/firebase'
 
 const ProfileSettings = () => {
+  const [user] = useAuthState(auth)
+  const { authState } = useContext(AuthStateContext)
+  const { email } = authState.user
+
+  const authorized = useAuthorization()
+
+  if (authorized === null) {
+    return (
+      <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-white">
+        <div className="text-black">Loading...</div>
+      </div>
+    )
+  }
+
   return (
     <>
-      <Layout> </Layout>
+      {authorized ? (
+        <>
+          <Layout>
+            <div className="flex items-center justify-start min-h-screen pt-16">
+              <div className="absolute top-0 left-0 w-full h-full bg-white flex items-center justify-center">
+                <div className="relative w-4/5 bg-white border-none flex flex-wrap flex-col items-start">
+                  <div className="flex items-center mr-2">
+                    {authState.provider === 'firebaseGoogle ' ? (
+                      <>
+                        <img
+                          src={user?.photoURL || ''}
+                          width="50"
+                          height="50"
+                          referrerPolicy="no-referrer"
+                          className="rounded-md transform hover:scale-110 transition-all duration-300 cursor-pointer"
+                        ></img>
+                      </>
+                    ) : (
+                      <>
+                        <AccountCircleIcon
+                          fontSize="inherit"
+                          color="primary"
+                          sx={{ fontSize: '100px' }}
+                        ></AccountCircleIcon>
+                      </>
+                    )}
+                    <div className="flex flex-col ml-4">
+                      <p className="font-bold text-black text-3xl mb-1">
+                        {email}
+                      </p>
+                      <p className="text-slate-500 text-lg">
+                        Member since unknown
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex items-center mt-2 px-24">
+                    <p className="font-bold text-black text-3xl mr-2">
+                      Sign-in method:
+                    </p>
+                    <p className="text-slate-500 text-xl mx-2">
+                      {authState.provider.charAt(0).toUpperCase() +
+                        authState.provider.slice(1)}
+                    </p>
+                    <Link href="/profile/settings/changepassword">
+                      <p className="text-black">Change Password</p>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Layout>
+        </>
+      ) : (
+        <>
+          <Forbidden />
+        </>
+      )}
     </>
   )
 }
--- src/hooks/useAuthorization.tsx
+++ src/hooks/useAuthorization.tsx
@@ -2,12 +2,10 @@ import { useContext, useEffect, useState } from 'react'
 import { AuthStateContext } from '@/context/AuthContext'
 
 export const useAuthorization = () => {
-  const [authorized, setAuthorized] = useState(false)
+  const [authorized, setAuthorized] = useState<any>(null)
   const { authState, getLoggedInUser } = useContext(AuthStateContext)
-  console.log(authState)
 
   useEffect(() => {
-    console.log('here')
     if (localStorage.getItem('isLoggedIn') === 'true') {
       const id = localStorage.getItem('id')
       getLoggedInUser(id)
