c654c27510de587a77123edc0206c6000a8ff00c
Xavier Hollingsworth <xavierelon93@gmail.com>
2023-05-02 18:20:32 -0400

    setting page styling

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -79,102 +79,118 @@ const Profile: NextPage = () => {
           <Layout>
             <div className="w-full bg-slate-100 py-4 mt-16 border-t-[#79589F] border-t-4">
               <div className="container mx-auto">
-                <p className="mx-16 text-md text-[#323B49]">Manage Account</p>
+                <div className="w-5/6 ml-auto">
+                  <p className="text-md text-[#323B49]">Manage Account</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="w-full bg-white py-4 mr-10 border-t-slate-200 border-b-slate-200 border-t-2 border-b-2">
+              <div className="container mx-auto ">
+                <div className="w-5/6 ml-auto">
+                  <ul className="flex space-x-8">
+                    <li className="text-gray-500 text-sm">Account</li>
+                    <li className="text-gray-500 text-sm">Settings</li>
+                    <li className="text-gray-500 text-sm">Payment</li>
+                  </ul>
+                </div>
               </div>
             </div>
 
             <div className="mx-auto bg-white min-h-screen min-w-screen">
-              <div className="flex flex-col h-full w-full">
-                <div className="flex mb-4 pt-20">
-                  {provider === 'firebaseGoogle' ? (
-                    <>
-                      <img
-                        src={authState.user?.photoURL || ''}
-                        width="100"
-                        height="100"
-                        referrerPolicy="no-referrer"
-                        className="rounded-md transform hover:scale-110 transition-all duration-300 cursor-pointer"
-                      ></img>
-                    </>
-                  ) : (
-                    <>
-                      <div
-                        onMouseEnter={() => setIsHovering(true)}
-                        onMouseLeave={() => setIsHovering(false)}
-                        className="relative"
-                      >
-                        {imageUrl ? (
+              <div className="flex flex-col h-full w-full container mx-auto">
+                <div className="w-5/6 ml-auto">
+                  <div className="flex mb-4 pt-20">
+                    {provider === 'firebaseGoogle' ? (
+                      <>
+                        <img
+                          src={authState.user?.photoURL || ''}
+                          width="100"
+                          height="100"
+                          referrerPolicy="no-referrer"
+                          className="rounded-md transform hover:scale-110 transition-all duration-300 cursor-pointer"
+                        ></img>
+                      </>
+                    ) : (
+                      <>
+                        <div
+                          onMouseEnter={() => setIsHovering(true)}
+                          onMouseLeave={() => setIsHovering(false)}
+                          className="relative"
+                        >
+                          {imageUrl ? (
+                            <>
+                              <div className="w-[100px] h-[100px] rounded-full overflow-hidden">
+                                <img
+                                  src={imageUrl}
+                                  alt="profilePicture"
+                                  className="w-full h-full rounded-full"
+                                ></img>
+                              </div>
+                            </>
+                          ) : (
+                            <AccountCircleIcon
+                              fontSize="inherit"
+                              color="primary"
+                              sx={{ fontSize: '100px' }}
+                            ></AccountCircleIcon>
+                          )}
+
                           <>
-                            <div className="w-[100px] h-[100px] rounded-full overflow-hidden">
-                              <img
-                                src={imageUrl}
-                                alt="profilePicture"
-                                className="w-full h-full rounded-full"
-                              ></img>
-                            </div>
+                            <form>
+                              <input
+                                type="file"
+                                accept="image/*"
+                                name="file"
+                                onChange={handleFileUpload}
+                                className="hidden"
+                                id="fileInput"
+                              ></input>
+
+                              <label htmlFor="fileInput">
+                                {isHovering && (
+                                  <FileUploadIcon
+                                    fontSize="inherit"
+                                    color="primary"
+                                    sx={{
+                                      fontSize: '40px',
+                                      position: 'absolute',
+                                      top: '80%',
+                                      left: '50%',
+                                      transform: 'translate(-50%, -50%)',
+                                      backgroundColor: 'white',
+                                      borderRadius: '50px',
+                                      cursor: 'pointer',
+                                    }}
+                                  />
+                                )}
+                              </label>
+                            </form>
                           </>
-                        ) : (
-                          <AccountCircleIcon
-                            fontSize="inherit"
-                            color="primary"
-                            sx={{ fontSize: '100px' }}
-                          ></AccountCircleIcon>
-                        )}
+                        </div>
+                      </>
+                    )}
 
-                        <>
-                          <form>
-                            <input
-                              type="file"
-                              accept="image/*"
-                              name="file"
-                              onChange={handleFileUpload}
-                              className="hidden"
-                              id="fileInput"
-                            ></input>
-
-                            <label htmlFor="fileInput">
-                              {isHovering && (
-                                <FileUploadIcon
-                                  fontSize="inherit"
-                                  color="primary"
-                                  sx={{
-                                    fontSize: '40px',
-                                    position: 'absolute',
-                                    top: '80%',
-                                    left: '50%',
-                                    transform: 'translate(-50%, -50%)',
-                                    backgroundColor: 'white',
-                                    borderRadius: '50px',
-                                    cursor: 'pointer',
-                                  }}
-                                />
-                              )}
-                            </label>
-                          </form>
-                        </>
-                      </div>
-                    </>
-                  )}
-
-                  <div className="flex flex-col ml-4">
-                    <p className="font-bold text-black text-3xl mb-1">
-                      {email}
-                    </p>
+                    <div className="flex flex-col ml-4">
+                      <p className="font-bold text-black text-3xl mb-1">
+                        {email}
+                      </p>
+                    </div>
                   </div>
-                </div>
-                <div className="flex items-center mt-2 px-24">
-                  <p className="font-bold text-black text-3xl mr-2">
-                    Sign-in method:
-                  </p>
-                  <p className="text-slate-500 text-xl mx-2">
-                    {authState.provider.charAt(0).toUpperCase() +
-                      authState.provider.slice(1)}
-                  </p>
-                  {provider === 'local' && (
-                    <Link href="/profile/settings/changepassword">
-                      <p className="text-black">Change Password</p>
-                    </Link>
-                  )}
+                  <div className="flex items-center mt-2 px-24">
+                    <p className="font-bold text-black text-3xl mr-2">
+                      Sign-in method:
+                    </p>
+                    <p className="text-slate-500 text-xl mx-2">
+                      {authState.provider.charAt(0).toUpperCase() +
+                        authState.provider.slice(1)}
+                    </p>
+                    {provider === 'local' && (
+                      <Link href="/profile/settings/changepassword">
+                        <p className="text-black">Change Password</p>
+                      </Link>
+                    )}
+                  </div>
                 </div>
               </div>
             </div>
--- src/components/navigation/Footer.tsx
+++ src/components/navigation/Footer.tsx
@@ -1,26 +1,29 @@
 'use client'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export const Footer = () => {
   return (
     <>
-      <div className="w-full bg-slate-100 py-4 mt-auto h-24">
+      <div className="w-full bg-slate-100 py-4 mt-auto h-16">
         <div className="w-full text-center h-full">
           <div className="flex justify-between items-center h-full px-2 md:px-4 lg:px-6">
             <div className="flex items-center">
               <ul className="flex space-x-4">
-                <li className="text-gray-500">Item 1</li>
-                <li className="text-gray-500">Item 2</li>
-                <li className="text-gray-500">Item 3</li>
-                <li className="text-gray-500">Item 4</li>
+                <li className="text-gray-500 text-sm">
+                  <Link href="/">xsj.com</Link>
+                </li>
+                <li className="text-gray-400 text-sm">Blog</li>
+                <li className="text-gray-500 text-sm">Careers</li>
+                <li className="text-gray-500 text-sm">Documentation</li>
               </ul>
             </div>
             <div className="flex items-center">
               <ul className="flex space-x-4">
-                <li className="text-gray-500">Item 5</li>
-                <li className="text-gray-500">Item 6</li>
-                <li className="text-gray-500">Item 7</li>
-                <li className="text-gray-500">Item 8</li>
+                <li className="text-gray-500 text-sm">Terms of Service</li>
+                <li className="text-gray-500 text-sm">Privacy</li>
+                <li className="text-gray-500 text-sm">Cookies</li>
+                <li className="text-gray-500 text-sm">© 2023 xsj.com</li>
               </ul>
             </div>
           </div>
