6180ef82e2ce74e8b2e541afcfeaf48c8a526b98
Xavier Hollingsworth <xavierelon93@gmail.com>
2023-05-09 14:55:27 -0400

    fix

--- src/app/auth/route.ts
+++ src/app/auth/route.ts
@@ -3,6 +3,6 @@ import { sign, verify } from 'jsonwebtoken'
 
 export async function GET(request: Request, response: Response) {
     const accessToken = getCookie('access-token')
-    return new Response(accessToken)
+    // return new Response(accessToken)
   }
   
