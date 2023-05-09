2807fc81ca5d9f2a63bcaf809c4f4a63e0e72b47
Xavier Hollingsworth <xavierelon93@gmail.com>
2023-05-09 14:37:48 -0400

    set cookie on server

--- src/components/modals/LoginModal.tsx
+++ src/components/modals/LoginModal.tsx
@@ -50,7 +50,11 @@ const LoginModal = (props: any) => {
           id: response.data.user._id,
         })
 
-        // setCookie('access-token', response.data.accessToken)
+        setCookie('access-token', response.data.accessToken, {
+          maxAge: 60 * 60 * 24,
+          httpOnly: true,
+          secure: true,
+        })
         sessionStorage.setItem('id', response.data.user._id)
         sessionStorage.setItem('isLoggedIn', 'true')
         sessionStorage.setItem('firstName', response.data.user.local.firstName)
