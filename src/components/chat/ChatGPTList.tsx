d5e8bb402da2dbed3a49741047dbc64450e161ad
Xavier <xavierh93@hotmail.com>
2023-06-22 12:20:10 -0400

    chat gpt convos updating in ui

--- src/components/chat/ChatGPTList.tsx
+++ src/components/chat/ChatGPTList.tsx
@@ -1,37 +1,30 @@
 'use client'
 import { useContext, useEffect, useRef, useState } from 'react'
-import axios from 'axios'
-import Image from 'next/image'
-import AccountCircleIcon from '@mui/icons-material/AccountCircle'
-import Tooltip from '@mui/material/Tooltip'
-import SmartToyIcon from '@mui/icons-material/SmartToy'
 import { AuthStateContext } from '@/context/AuthContext'
 import { ChatStateContext } from '@/context/ChatContext'
 import { ConversationInterface } from '@/models/chat.interfaces'
 import { createChatGPTConversation, getUsersConversations } from '@/firebase/chat.firebase'
-import firebase from 'firebase/compat/app'
 import 'firebase/compat/firestore'
 import './UsersList.css'
 
 const ChatGPTList = () => {
   const [chatGPTConversations, setChatGPTConversations] = useState<any[]>([])
-  const { conversations, setCurrentConversation, setCurrentConversationID, chatGPTConversation, setChatGPTConversation } =
+  const { conversations, setCurrentConversation, setCurrentConversationID, chatGPTConversation, setChatGPTConversation, setConversations } =
     useContext(ChatStateContext)
   const { authState } = useContext(AuthStateContext)
   const { id } = authState
-
-  // let chats = [{ name: 'achilles' }, { name: 'musk' }, { anme: 'flocka' }]
 
   const getConversationsWithChatGPT = (conversations: ConversationInterface[]): ConversationInterface[] => {
     const chatGPTConversations = conversations.filter((conversation) => conversation.users.includes('chatGPT-3.5'))
     return chatGPTConversations ? chatGPTConversations : []
   }
 
-  const handleChatGPTClick = async () => {
+  const handleNewChatGPTClick = async () => {
     if (!chatGPTConversation) {
       try {
         const conversationID = await createChatGPTConversation(id)
         const convos = await getUsersConversations(id)
+        setConversations(convos)
         console.log(conversationID)
         setCurrentConversation(null)
         setCurrentConversationID(conversationID)
@@ -69,7 +62,7 @@ const ChatGPTList = () => {
       <div
         className="flex justify-center border-white rounded-xl border-4 py-3 mx-4
     cursor-pointer mb-5"
-        onClick={handleChatGPTClick}
+        onClick={handleNewChatGPTClick}
       >
         <p className="justify-center text-black text-xl">New Chat</p>
       </div>
