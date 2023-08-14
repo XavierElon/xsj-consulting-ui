f4c6906205186d873263c9ab2ed9ab3a37895087
Xavier <xavierh93@hotmail.com>
2023-08-14 18:02:44 -0400

    update packages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,7 @@ import { NextPage } from 'next'
 import React, { useContext, useEffect } from 'react'
 import axios from 'axios'
 import Layout from '@/components/Layout'
-import {
-  CardOne,
-  CardTwo,
-  CardThree,
-  CityCard,
-  TableCard,
-} from '@/components/cards'
+import { CardOne, CardTwo, CardThree, CityCard, TableCard } from '@/components/cards'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import { AuthStateContext } from '@/context/AuthContext'
@@ -22,9 +16,9 @@ const HomePage: NextPage = (Component, pageProps) => {
       <div className="stars" />
       <Layout>
         <ToastContainer autoClose={1000} />
-        <div className="row-span-1 py-20">
+        {/* <div className="row-span-1 py-20">
           <CardOne />
-        </div>
+        </div> */}
 
         <CityCard />
 
