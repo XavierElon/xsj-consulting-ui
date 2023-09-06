6686215d70c6de23f0b7e6f90bef179c53b1b8d8
Xavier <xavierh93@hotmail.com>
2023-09-06 12:07:01 -0400

    delete working here

--- machines/todoMachine.ts
+++ machines/todoMachine.ts
@@ -1,23 +1,24 @@
 import { createMachine, assign } from 'xstate'
 
+type TodoEvents = { type: 'Create New' } | { type: 'Form input changed'; value: string } | { type: 'Submit' } | { type: 'Delete'; todo: string }
+
 export const todoMachine = createMachine(
   {
-    /** @xstate-layout N4IgpgJg5mDOIC5QBUD2FUAICyBDAxgBYCWAdmAHQAyquEZUmaGsAxBuRWQG6oDWlZljxEylGnQZN0qWAh6p8uAC7FUpANoAGALradiUAAdZxVesMgAHogDMAFgDsFAEwBWewDYAnN8db-WwBGdwAaEABPRCC3Ci0vAA5HNwBfFPChHAISTgl6UkYhNjAAJxLUEoojABsVADMKgFsKTJEc8Vp8wpk5BSVzTV19SxNYMzVSSxsEAFovV08g+MdbT1sXBJC3cKiEbyCKezdHBJcnAK1g9zSMmSzRTiLMPMhWAGESsBUwTHIAd2GSBAo3GFiB03sCQSFG8bkWti03k29iOth2dhicThQU8jk8Wnctm8Wk8NxArWyYgoHy+qgKmAAcmA-tIMBQAMqEVB-KQNEqNTBkIwAV2UrAAYk1BaQRcpMERcAVIIDjKYBlNEMcDojfC4kYjjpd0QgPActlo3A4LXigo40ukQKR0HBLBSHmARmqJhrZt57Aslk5VutNmFIogZhsXHF8UjvLZHEjTgkyW72tROlIip6xurwRHPIdbG4Em4XBaAqHtuHZkE-RRjg4EmsNltU3c2lSni8IDnQZN8wh7MSG4iiX6gvYSWHdratBRPJs6-FiY4XC5PKSHWmqXksz1MABRMoVSB9vOgaZaY1uLTQpHJS4W23HFPbjuUzg0lRSJksoTnt6g6QrYhxaJOCTeO4JxbMaRLRkECZuLexZTpc3jthg9zpt+dKMH+rKoByXI8vSfICkKoqAWCl6asEYEQVBr6wTWTjQmsiHgUkSYbPaKRAA */
+    /** @xstate-layout N4IgpgJg5mDOIC5QBUD2FUAICyBDAxgBYCWAdmAHQAyquEZUmaGsAxBuRWQG6oDWlZljxEylGnQZN0qWAh6p8uAC7FUpANoAGALradiUAAdZxVesMgAHogC0AFgCcFewCYnAVgDsrrx4DMjloAbK4eADQgAJ52YV4UwQEAjCHBAByO9skAvtmRQjgEJJwS9KSMQmxgAE7VqNUURgA2KgBm9QC2FAUixeK0ZRUycgpK5pq6+pYmsGZqpJY2CA5uFJlaXlr2-lmb-kmRMctJaUkU7q5JHsGOaRcnabn5MoWinJWYpZCsAMLVYCowJgAHJgADuUyQIBmcwsUKW-hCFH8aW26QywXsqPshzsSSSrgoAS89j8HjSHkc-j8TxAPSKYm6w0+A2+ABEwE0wMowJDjKZxotEB5XFpzltgul0lo7jdccsdmLtgTXGlgtSbgFafS3pQ-gDVOUQeDpBgKABlQioMFSdrVDqYMhGACuylYADFOo7SC7lJgiLhypA+dCBfMhQhXIiEptfKrfEksfYcdFEE4zqqvKEtB5k9t-MFtS9eoz9SopKCwabUBarTajXaHU7Xaxzc6AEYdMwhmGC+GIQLxLYa1yivYEiKphD+UXIsIpPwk+yhRxFjCvPoUMuGxiV6sW3DcKRCdjqSgKARM9clzjbismoQHo9GoTyUi8MbzfQ9sNw0BLewtH8NZ0i8QI7hJMDMnlWxZyxDwQnsB51UcVUPDXYQGVvf5yyNPdH3NQ9jxkVgajqBpmjaTor0w3UtxwndjSrAiiJfGQ3w-ct1G-XRpl-BZ+wQa57GRa5-ERNIvCuO55UCYDyUcW5JJlJxgiSDCN0ZDkuUYk8OHPd9+EEYssMobTuWIjAOMULiJj0XioV7cNBJSNIKBOLQtCSRFRTScTXGCGCRXiElsQLICPCuQs8jpEy6PM3SSLI+pGhaZRGxozTOASyzUGsz9uMmBz+VmPt-0QdUPAoU4Un8SLrjCGCkkcKqsUcTUo3xZqvEeWlSHQOBLB1Po+NK5zyuWJwxXWTZtl2ICDinWw1KqqNKWTfFLiQ1cYuGxlSly+BHP4iMHCA9yrgQoJ-IWmDl3iXwdmlJJF1ODSb2MlgWToSBRthASJsueIkmCLRMjHLMCx8GCfPc1Ucw8bwwjBrwvHe0zqAGQ7MAAUVqepfuOsa-2sOxAmCGN2uuW57kceUowpzMRW80JEVJdG6LvPCHxkP6ytJhBE2cTF0XxCKotceVuvcrxrsCK5RROewOc3Lndx5s1LWtW0vWbZQ+fGgXpZFtIavFhrZOaigNjufYapFHZld2uLVYY+9mJkJ9coNkmlmCKSKCzFIQdRbxbqnVURMlRJHajO43ud68MZytiMB9gGBdCNzQkuVCUVVCc7v984wOXNIvNe9TE9ozcU8YZQXmS-4IHTiNMXiRwoqcJDANCScjmWkTFMpRFbhavydlyXIgA */
     id: 'Todo Machine',
     initial: 'Loading Todos',
     schema: {
-      events: {} as
-        | {
-            type: 'Create New'
-          }
-        | {
-            type: 'Form input changed'
-            value: string
-          },
+      events: {} as TodoEvents,
       // events: {} as { type: 'Todos Loaded'; todos: string[] } | { type: 'Loading todos failed'; errorMessage: string }
       services: {} as {
         loadTodos: {
           data: string[]
+        }
+        saveTodo: {
+          data: void
+        }
+        deleteTodo: {
+          data: void
         }
       }
     },
@@ -57,7 +58,12 @@ export const todoMachine = createMachine(
 
       'Todos Loaded': {
         on: {
-          'Create New': 'Creating New Todo'
+          'Create New': {
+            target: 'Creating New Todo'
+          },
+          Delete: {
+            target: 'Deleting Todo'
+          }
         }
       },
 
@@ -70,11 +76,38 @@ export const todoMachine = createMachine(
             on: {
               'Form input changed': {
                 actions: 'assignInputFormToContext'
-              }
+              },
+
+              Submit: 'Saving Todo'
+            }
+          },
+
+          'Saving Todo': {
+            invoke: {
+              src: 'saveTodo',
+
+              onError: [
+                {
+                  target: 'Showing form input',
+                  actions: 'assignErrorToContext'
+                }
+              ],
+
+              onDone: '#Todo Machine.Loading Todos'
             }
           }
         }
-      }
+      },
+
+      'Deleting Todo': {
+        invoke: {
+          src: 'deleteTodo',
+          onError: 'Deleting todo errored',
+          onDone: 'Loading Todos'
+        }
+      },
+
+      'Deleting todo errored': {}
     }
   },
   {
--- src/app/xstate/page.tsx
+++ src/app/xstate/page.tsx
@@ -4,11 +4,23 @@ import Layout from '@/components/Layout'
 import { useMachine } from '@xstate/react'
 import { todoMachine } from 'machines/todoMachine'
 
+const todos = new Set<string>(['Take bins out', 'Do laundry'])
+
 const XState: NextPage = () => {
   const [state, send] = useMachine(todoMachine, {
     services: {
       loadTodos: async () => {
-        return ['Take bins out', 'Do laundry']
+        return Array.from(todos)
+      },
+      saveTodo: async (context, event) => {
+        todos.add(context.createNewTodoFormInput)
+      },
+      deleteTodo: async (context, event) => {
+        console.log(event.type)
+        if (event.type === 'Delete') {
+          console.log('here')
+          todos.delete(event.todo)
+        }
       }
     }
   })
@@ -22,6 +34,22 @@ const XState: NextPage = () => {
             <pre>{JSON.stringify(state.value)}</pre>
             <pre>{JSON.stringify(state.context)}</pre>
             <div>
+              {state.context.todos.map((todo) => (
+                <div key={todo} className="flex items-center">
+                  <p>{todo}</p>
+                  <button
+                    onClick={() => {
+                      send({
+                        type: 'Delete',
+                        todo
+                      })
+                    }}
+                    className="ml-2 bg-slate-500"
+                  >
+                    Delete
+                  </button>
+                </div>
+              ))}
               {state.matches('Todos Loaded') && (
                 <button
                   onClick={() => {
@@ -34,12 +62,21 @@ const XState: NextPage = () => {
                 </button>
               )}
               {state.matches('Creating New Todo.Showing form input') && (
-                <input
-                  className="bg-white"
-                  onChange={(e) => {
-                    send({ type: 'Form input changed', value: e.target.value })
+                <form
+                  onSubmit={(e) => {
+                    e.preventDefault()
+                    send({
+                      type: 'Submit'
+                    })
                   }}
-                ></input>
+                >
+                  <input
+                    className="bg-white"
+                    onChange={(e) => {
+                      send({ type: 'Form input changed', value: e.target.value })
+                    }}
+                  ></input>
+                </form>
               )}
             </div>
           </div>
